7d4a01a3956551ad5bc8a442cec5b6fdcf499271
temaput <putilkin@gmail.com>
2023-09-28 23:08:41 -0300

    Solve serialization issues

--- robokassa/facade.py
+++ robokassa/facade.py
@@ -3,19 +3,23 @@
 django-robokassa"""
 
 from logging import getLogger
+
 log = getLogger('robokassa.facade')
 
+import json
+from urllib import quote
 from django.core.urlresolvers import reverse
 from oscar.core.loading import get_class
 
-
-
 from robokassa.forms import RobokassaForm
 from robokassa.conf import EXTRA_PARAMS, ROBOKASSA_SESSION_KEY
-OPTIONAL_PARAMS = ('Desc', 'IncCurrLabel', 'Email', 'Culture')
-RedirectRequired = get_class('payment.exceptions','RedirectRequired')
+from robokassa.utils import DecimalMonetaryEncoder
 
-def robokassa_redirect(request, basket_num, amount, receiptDTO = "", **kwargs):
+OPTIONAL_PARAMS = ('Desc', 'IncCurrLabel', 'Email', 'Culture')
+RedirectRequired = get_class('payment.exceptions', 'RedirectRequired')
+
+
+def robokassa_redirect(request, basket_num, amount, receiptDTO=None, **kwargs):
     """ This will be called from PaymentDetailsView.handle_payment,
     it supposed to generate url for Robokassa, inject it into RedirectRequired
     error and raise it
@@ -24,26 +28,42 @@ def robokassa_redirect(request, basket_num, amount, receiptDTO = "", **kwargs):
 
     """
 
+    if receiptDTO is not None:
+        receiptDTO = json.dumps(receiptDTO, cls=DecimalMonetaryEncoder)
+        receiptDTO = quote(receiptDTO)
 
+        initial = {
+            'OutSum': float(round(amount, 2)),
+            'InvId': basket_num,
+            'Receipt': receiptDTO
+        }
+        for key in kwargs:
+            if key in EXTRA_PARAMS or key in OPTIONAL_PARAMS:
+                initial[key] = kwargs[key]
 
-    initial={'OutSum': amount, 'InvId': basket_num, 'Receipt': receiptDTO}
-    for key in kwargs:
-        if key in EXTRA_PARAMS or key in OPTIONAL_PARAMS:
-            initial[key] = kwargs[key]
+        session = request.session
+        session[ROBOKASSA_SESSION_KEY] = initial
+        session.save()
+        url = reverse('redirect')
+        err = RedirectRequired(url)
+        raise err
+    else:
+        initial = {
+            'OutSum': float(round(amount, 2)),
+            'InvId': basket_num,
+        }
+        for key in kwargs:
+            if key in EXTRA_PARAMS or key in OPTIONAL_PARAMS:
+                initial[key] = kwargs[key]
 
-    session = request.session
-    session[ROBOKASSA_SESSION_KEY] = initial
-    session.save()
-    url = reverse('redirect')
-    # if session.session_key is not None:
-    #     initial['session_key'] = session.session_key
-    # else:
-    #     log.error('session_key is empty')
+        session = request.session
+        session.save()
+        if session.session_key is not None:
+            initial['session_key'] = session.session_key
+        else:
+            log.error('session_key is empty')
 
-    # form = RobokassaForm(initial=initial)
-    err = RedirectRequired(url)
-    raise err
-
-
-
+        form = RobokassaForm(initial=initial)
+        err = RedirectRequired(form.get_redirect_url())
+        raise err
 
